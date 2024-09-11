3c9b801464c41396a66cb79fabc901728e47d71a
Sam Horsfield <s.horsfield19@imperial.ac.uk>
2024-09-11 11:27:48 +0100

    Adds sequence concatenation

--- panPrompt.py
+++ panPrompt.py
@@ -14,7 +14,7 @@ from multiprocessing import Manager
 import torch.multiprocessing as mp
 from panGPT import setup, cleanup, load_dataset
 import random
-from panGPT import GenomeDataset
+from panGPT import mask_integers
 from torch.utils.data import DataLoader, DistributedSampler
 
 logging.set_verbosity_error()
@@ -55,59 +55,123 @@ def parse_args():
 
     return args
 
-def pad_blocks(input_list, block_size, pad_token="<pad>", label=False):
-    # Initialize the result list
-    result = []
-    attention_mask = []
+def pad_input(input, max_length, pad_token_id, labels=False):
 
-    # Iterate over the list in steps of block_size
-    for i in range(0, len(input_list), block_size):
-        # Get the current block
-        block = input_list[i:i + block_size]
-        attention_block = [1] * len(block)
-        
-        # Check if the block needs padding
-        if len(block) < block_size:
-            # Pad the block to the required block size
-            attention_block += [0] * (block_size - len(block))
-            if label:
-                block += [-100] * (block_size - len(block))    
-            else:
-                block += [pad_token] * (block_size - len(block))            
-        
-        # Add the block to the result
-        result.append(block)
-        attention_mask.append(attention_block)
-    
-    return result, attention_mask
+    len_masked = len(input)
+    # only pad if necessary
+    if len_masked >= max_length:
+        pass
+    else:
+        if labels == False:
+            input.extend([pad_token_id] * (max_length - len_masked))
+        else:
+            input.extend([-100] * (max_length - len_masked))
 
-# returns list first entry is encoded, second is attention mask
-def tokenize_prompt(prompt, max_seq_length, tokenizer):
-    mask_token = tokenizer.encode("<mask>").ids[0]
-    pad_token = tokenizer.encode("<pad>").ids[0]
+    return input
 
-    encoded = tokenizer.encode(prompt).ids
+class GenomeDataset(torch.utils.data.Dataset):
+    """
+    Dataset class for genomic data.
 
-    # merge consecutive masks into single mask token
-    encoder_input = ' '.join([str(i) for i in encoded])
-    #print('encoder_input pre merging')
-    #print(encoder_input)
-    pattern = f'({mask_token} )+'
-    encoder_input = re.sub(pattern, str(mask_token) + ' ', encoder_input)
-    pattern = f'( {mask_token})+'
-    encoder_input = re.sub(pattern, ' ' + str(mask_token), encoder_input)
+    This class represents a dataset of genomic sequences for training, validation, or testing.
 
-    encoder_input = [int(i) for i in encoder_input.split()]
+    Args:
+    - texts (list): List of genomic sequences.
+    - tokenizer (Tokenizer): Tokenizer for encoding genomic sequences.
+    - max_length (int): Maximum length of the input sequence.
 
-    #print('encoder_input post merging')
-    #print(encoder_input)
+    Methods:
+    - __len__(): Get the length of the dataset.
+    - __getitem__(idx): Get an item from the dataset by index.
+    """
 
-    encoder_input, attention_mask = pad_blocks(encoder_input, max_seq_length, pad_token)
+    def __init__(self, texts, tokenizer, max_length, prop_masked):
+        self.tokenizer = tokenizer
+        self.texts = texts
+        self.max_length = max_length
+        self.prop_masked = prop_masked
+        self.mask_token = self.tokenizer.encode("<mask>").ids[0]
+        self.pad_token = self.tokenizer.encode("<pad>").ids[0]
+        self.bos_token = self.tokenizer.encode("<s>").ids[0]
+        self.eos_token = self.tokenizer.encode("</s>").ids[0]
 
-    #print('encoder_input post padding')
-    #print(encoder_input)
+    def __len__(self):
+        return len(self.texts)
 
-    return encoder_input, attention_mask
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+
+        input = self.tokenizer.encode(text).ids
+
+        # generate decoder and labels input, wrapping decoder input to right
+        labels = input[1:]
+
+        # mask original text
+        text = self.tokenizer.decode(labels, skip_special_tokens=False)
+        text_masked = mask_integers(text, self.prop_masked)
+
+        encoder_input = self.tokenizer.encode(text_masked).ids
+        decoder_input = input[:-1]
+
+        len_decoder = len(decoder_input)
+        decoder_input = pad_input(decoder_input, self.max_length, self.pad_token)
+
+        decoder_attention_mask = torch.ones(len(decoder_input), dtype=torch.long)
+        decoder_attention_mask[len_decoder:] = 0
+
+        labels = pad_input(labels, self.max_length, self.pad_token, labels=True)
+
+        # merge consecutive masks into single mask token
+        encoder_input = ' '.join([str(i) for i in encoder_input])
+        #print('encoder_input pre merging')
+        #print(encoder_input)
+        pattern = f'({self.mask_token} )+'
+        encoder_input = re.sub(pattern, str(self.mask_token) + ' ', encoder_input)
+        pattern = f'( {self.mask_token})+'
+        encoder_input = re.sub(pattern, ' ' + str(self.mask_token), encoder_input)
+        #print('encoder_input post merging')
+        #print(encoder_input)
+        encoder_input = [int(i) for i in encoder_input.split()]
+
+        len_masked = len(encoder_input)
+        encoder_input = pad_input(encoder_input, self.max_length, self.pad_token)
+
+        #print('encoder_input post padding')
+        #print(encoder_input)
+
+        # do not attend to mask tokens
+        #print(int(self.mask_token))
+        #mask_idx = np.flatnonzero(np.array(encoder_input) == int(self.mask_token))
+
+        encoder_attention_mask = torch.ones(len(encoder_input), dtype=torch.long)
+        encoder_attention_mask[len_masked:] = 0
+        #encoder_attention_mask[mask_idx] = 0
+
+        # attend to all contig breaks
+        global_attention_mask = torch.zeros(len(encoder_input), dtype=torch.long)
+        break_idx = np.flatnonzero(np.array(encoder_input) == int(self.tokenizer.encode("_").ids[0]))
+        global_attention_mask[break_idx] = 1
+
+        # print("labels")
+        # print(len(labels))
+        # print(labels)
+        # print("decoder_input")
+        # print(len(decoder_input))
+        # print(decoder_input)
+        # print("decoder_attention_mask")
+        # print(len(decoder_attention_mask.tolist()))
+        # print(decoder_attention_mask.tolist())
+        # print("encoder_input")
+        # print(len(encoder_input))
+        # print(encoder_input)
+        # print("encoder_attention_mask")
+        # print(len(encoder_attention_mask.tolist()))
+        # print(encoder_attention_mask.tolist())
+        # print("global_attention_mask")
+        # print(len(global_attention_mask.tolist()))
+        # print(global_attention_mask.tolist())
+
+        return torch.tensor(decoder_input, dtype=torch.long), torch.tensor(encoder_input, dtype=torch.long), torch.tensor(labels, dtype=torch.long), decoder_attention_mask, encoder_attention_mask, global_attention_mask
 
 def print_banner():
     banner = '''
@@ -140,27 +204,6 @@ def load_model(embed_dim, num_heads, num_layers, max_seq_length, device, vocab_s
     model = LEDForConditionalGeneration(BARTlongformer_config)
     return model
 
-def mask_integers(string, prop_masked):   
-    # Identify the indices of the integers in the list
-    integer_indices = np.array(string.split())
-    
-    # Determine how many integers to mask
-    num_to_mask = int(len(integer_indices) * prop_masked)
-    
-    # Randomly select indices to mask
-    if num_to_mask > 0:
-        indices_to_mask = np.random.choice(range(len(integer_indices)), size=num_to_mask, replace=False)
-    else:
-        indices_to_mask = np.empty(shape=[0, 0])
-    
-    # Replace selected indices with "[MASK]"
-    integer_indices[indices_to_mask] = "<mask>"
-
-    # Reconstruct the string
-    masked_string = ' '.join(integer_indices.tolist())
-    
-    return masked_string
-
 def predict_next_tokens_BART(model, tokenizer, prompt, loader, device, batch_size, temperature, prop_masked, num_seq, max_seq_length, DDP_active):
     model.eval()
     criterion = torch.nn.CrossEntropyLoss().to(device)
@@ -171,61 +214,66 @@ def predict_next_tokens_BART(model, tokenizer, prompt, loader, device, batch_siz
     total_accuracy = 0
     preds_all = []
     labels_all = []
+    predictions = []
     with torch.no_grad():
         for decoder_input, encoder_input, labels, decoder_attention_mask, encoder_attention_mask, global_attention_mask in loader:  # Correctly unpack the tuples returned by the DataLoader
-            decoder_input, encoder_input, decoder_attention_mask, encoder_attention_mask, global_attention_mask = decoder_input.to(device), encoder_input.to(device), decoder_attention_mask.to(device), encoder_attention_mask.to(device), global_attention_mask.to(device)  # Move data to the appropriate device
+  
+            total_len = encoder_input.size(1)
+            print(total_len)
+            current_sequence = []
+            for i in range(0, total_len, max_seq_length):
+                print("max_seq_length exceeded: {}".format(total_len > max_seq_length))
 
-            outputs = model(input_ids=encoder_input, attention_mask=encoder_attention_mask, decoder_input_ids=decoder_input, decoder_attention_mask=decoder_attention_mask, global_attention_mask=global_attention_mask).logits  # Generate predictions
-            #outputs = model(input_ids=encoder_input, attention_mask=encoder_attention_mask, decoder_input_ids=decoder_input, decoder_attention_mask=decoder_attention_mask).logits
+                batch_decoder_input, batch_encoder_input, batch_decoder_attention_mask, batch_encoder_attention_mask, batch_global_attention_mask = decoder_input[:, i:i + max_seq_length].to(device), encoder_input[:, i:i + max_seq_length].to(device), decoder_attention_mask[:, i:i + max_seq_length].to(device), encoder_attention_mask[:, i:i + max_seq_length].to(device), global_attention_mask[:, i:i + max_seq_length].to(device) # Move data to the appropriate device
 
-            # Free GPU memory
-            del encoder_input
-            del encoder_attention_mask
-            del decoder_input
-            del decoder_attention_mask
-            del global_attention_mask
+                outputs = model(input_ids=batch_encoder_input, attention_mask=batch_encoder_attention_mask, decoder_input_ids=batch_decoder_input, decoder_attention_mask=batch_decoder_attention_mask, global_attention_mask=batch_global_attention_mask).logits  # Generate predictions
+                #outputs = model(input_ids=encoder_input, attention_mask=encoder_attention_mask, decoder_input_ids=decoder_input, decoder_attention_mask=decoder_attention_mask).logits
 
-            #torch.cuda.empty_cache()
+                # Free GPU memory
+                del encoder_input
+                del encoder_attention_mask
+                del decoder_input
+                del decoder_attention_mask
+                del global_attention_mask
 
-            labels = labels.to(device)
+                #torch.cuda.empty_cache()
+
+                batch_labels = labels[:, i:i + max_seq_length].to(device)
+                
+                loss = criterion(outputs.view(-1, vocab_size), batch_labels.view(-1))
+                total_val_loss += loss.item() * batch_labels.size(0)  # Accumulate the loss
+
+                preds = outputs.argmax(dim=-1)  # Get predicted classes
+
+                # ignore padding positions
+                mask = batch_labels != -100
+                preds = preds[mask]
+                batch_labels = batch_labels[mask]
+
+                # calculate accuracy
+                correct = (preds == batch_labels).sum().item()
+                accuracy = correct / batch_labels.numel()
+                total_accuracy += accuracy * batch_labels.size(0)  # Accumulate the accuracy
+
+                print("accuracy:")
+                print(accuracy)
+                print("loss:")
+                print(loss.item())
+                # print("preds:")
+                # print(preds.tolist())
+                # print("labels:")
+                # print(batch_labels.tolist())
+                print("matches:")
+                print((preds == batch_labels).type(torch.uint8).tolist())
+
+                print(preds)
+                current_sequence.append(preds)
             
-            loss = criterion(outputs.view(-1, vocab_size), labels.view(-1))
-            total_val_loss += loss.item() * labels.size(0)  # Accumulate the loss
-
-            preds = outputs.argmax(dim=-1)  # Get predicted classes
-
-            # ignore padding positions
-            mask = labels != -100
-            preds = preds[mask]
-            labels = labels[mask]
-
-            # calculate accuracy
-            correct = (preds == labels).sum().item()
-            accuracy = correct / labels.numel()
-            total_accuracy += accuracy * labels.size(0)  # Accumulate the accuracy
-
-            print("accuracy:")
-            print(accuracy)
-            print("loss:")
-            print(loss.item())
-            print("preds:")
-            print(preds.tolist())
-            print("labels:")
-            print(labels.tolist())
-            print("matches:")
-            print((preds == labels).type(torch.uint8).tolist())
-
-            #print("preds masked:")
-            #print(preds.tolist())
-            #print("label masked:")
-            #print(labels.tolist())
-
-            # Collect predictions and labels for calculating additional metrics
-            preds_all.extend(preds.view(-1).tolist())
-            labels_all.extend(labels.view(-1).tolist())
-            # Update the progress bar
-            #loader.update(1)
-            fail
+            # concatenate predictions
+            current_sequence = [tensor.unsqueeze(0) if tensor.ndim == 1 else tensor for tensor in current_sequence]
+            sequence = torch.cat(current_sequence, dim=1)
+            predictions.append(sequence)
+        print(predictions)
 
     #TODO need to make this so that is encodes input, splits to longest lengths, then decodes, masks for encoder and takes the first character from unmasked sequence for decoder
     encoded = tokenizer.encode(prompt).ids
